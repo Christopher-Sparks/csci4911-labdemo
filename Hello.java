d9bbd981fa775ade8c37b28038368749043a4190
Christopher-Sparks <csparks1@my.hpu.edu>
2019-01-20 18:11:56 -1000

    Added method

--- Hello.java
+++ Hello.java
@@ -1,6 +1,11 @@
 public class Hello{
     public static void main(String args[]){
-        System.out.println("Hello");
+        //System.out.println("Hello");
+        greeting("Hello");
+    }
+
+    public static void greeting(String message){
+        System.out.println(message);
     }
 }
 
